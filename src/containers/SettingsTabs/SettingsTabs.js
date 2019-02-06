0aeba24a6181e96598a2619a23e8e446ad2893d2
Bohdan Hushcha <hushchabogdan@gmail.com>
2019-02-06 16:19:53 +0100

    Setting up Settings tab. Before react-tabs

--- src/containers/SettingsTabs/SettingsTabs.js
+++ src/containers/SettingsTabs/SettingsTabs.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
-import { TabContent, TabPane, Nav, NavItem, NavLink } from "reactstrap";
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
 import classnames from "classnames";
 import classes from "./SettingsTabs.module.css";
+import "./SettingsTabs.css";
 
 class SettingsTabs extends Component {
   constructor(props) {
@@ -22,57 +32,63 @@ class SettingsTabs extends Component {
   }
   render() {
     return (
-      <div className={classes.Tab_Wrapper}>
-        <Nav tabs vertical className={classes.Nav}>
-          <NavItem>
-            <NavLink
-              className={
-                classnames({ active: this.state.activeTab === "1" }) +
-                classes.NavLink
-              }
-              onClick={() => {
-                this.toggle("1");
-              }}
+      <Container>
+        <Row>
+          <Col md="2">
+            <Nav tabs vertical className={classes.Nav}>
+              <NavItem>
+                <NavLink
+                  className={
+                    classnames({ active: this.state.activeTab === "1" }) +
+                    classes.NavLink
+                  }
+                  onClick={() => {
+                    this.toggle("1");
+                  }}
+                >
+                  Orders
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={
+                    classnames({ active: this.state.activeTab === "2" }) +
+                    classes.NavLink
+                  }
+                  onClick={() => {
+                    this.toggle("2");
+                  }}
+                >
+                  Cars
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={
+                    classnames({ active: this.state.activeTab === "3" }) +
+                    classes.NavLink
+                  }
+                  onClick={() => {
+                    this.toggle("3");
+                  }}
+                >
+                  Verify account
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Col>
+          <Col md="10">
+            <TabContent
+              activeTab={this.state.activeTab}
+              className={classes.Tab_Content}
             >
-              My orders
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={
-                classnames({ active: this.state.activeTab === "2" }) +
-                classes.NavLink
-              }
-              onClick={() => {
-                this.toggle("2");
-              }}
-            >
-              My cars
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={
-                classnames({ active: this.state.activeTab === "3" }) +
-                classes.NavLink
-              }
-              onClick={() => {
-                this.toggle("3");
-              }}
-            >
-              Verify account
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <TabContent
-          activeTab={this.state.activeTab}
-          className={classes.Tab_Content}
-        >
-          <TabPane tabId="1">Hello 1</TabPane>
-          <TabPane tabId="2">Hello 2</TabPane>
-          <TabPane tabId="3">Hello 3</TabPane>
-        </TabContent>
-      </div>
+              <TabPane tabId="1">Hello 1</TabPane>
+              <TabPane tabId="2">Hello 2</TabPane>
+              <TabPane tabId="3">Hello 3</TabPane>
+            </TabContent>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
